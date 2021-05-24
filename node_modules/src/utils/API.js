111ac851a83cf6e127fa677eb8ee25233a62cf0b
Farran S Campbell <Farranerinandcrew@gmail.com>
2021-05-24 10:24:21 -0400

    Add files via upload

---
+++ node_modules/src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+// fetches all users at random
+const API = {
+  getUsers: function () {
+    return axios.get("https://randomuser.me/api/?results=60&nat=us");
+  },
+};
+
+export default API;
